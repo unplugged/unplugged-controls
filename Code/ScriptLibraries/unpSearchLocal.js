21421652298a44f63aa02632003851581f833537
Matt White <matthew.white@fclonline.com>
2012-10-30 10:35:22 +0000

    Added local search support to header bar

--- Code/ScriptLibraries/unpSearchLocal.js
+++ Code/ScriptLibraries/unpSearchLocal.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
 	$('#input-search').keyup( function() {
-		$('table.data-row').css("display", "");
-			$('ul.data-row').css("display", "");
-			var searchterm = $('#input-search').val();
-			if (searchterm.length > 2) {
-				var nomatch = $('table.data-row:not(:contains("' + searchterm + '"))');
-				nomatch.css("display", "none");
-				var nomatch2 = $('ul.data-row:not(:contains("' + searchterm + '"))');
-				nomatch2.css("display", "none");
-			} else {
-				$('table.data-row').css("display", "");
-				$('ul.data-row').css("display", "");
-			}
+		$('li.data-row').css("display", "none");
+		var searchterm = $('#input-search').val();
+		console.log("Searching for: \"" + searchterm + "\"");
+		if (searchterm.length > 2) {
+			var nomatch2 = $('li.data-row:icontains("' + searchterm + '")');
+			console.log("Got " + nomatch2.length + " results");
+			nomatch2.css("display", "");
+		} else {
+			//$('ul.data-row').css("display", "");
+		}
 	});
 
-	jQuery.expr[':'].contains = function(a, i, m) {
+	jQuery.expr[':'].icontains = function(a, i, m) {
+		console.log("icontains(" + a + ", " + i + ", " + m + ")");
 		return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
 	};
 });
