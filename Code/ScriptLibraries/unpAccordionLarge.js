688e0cbe86ab9d59c0364058ce30d36b803f21b2
Rich Sharpe <richard_sharpe@teamstudio.com>
2012-11-28 01:18:35 -0500

    Reworking of Accordion

--- Code/ScriptLibraries/unpAccordionLarge.js
+++ Code/ScriptLibraries/unpAccordionLarge.js
@@ -1,20 +1,55 @@
 
-function fetchDetails(obj, viewName, catName)
+
+function accordionLoadMore(obj, viewName, catName, xpage){
+	
+	var thisArea = $(obj).nextAll(".summaryDataRow:first").children(".accordionRowSet");		
+	console.log("thisArea = " + $(thisArea).attr('class'));
+	var pos = $(thisArea).find('li').length;
+	console.log("pos length= " + $(thisArea).find('li').length);
+	thisArea.css('display','block');
+	var thisUrl = "UnpAccordionViewList.xsp?chosenView=" + encodeURIComponent(viewName) + "&catFilter=" + encodeURIComponent(catName) + "&xpageDoc=" + xpage + "&start=" + pos; 
+	
+	var tempHolder = $(obj).nextAll(".summaryDataRow:first").children(".summaryDataRowHolder");
+	$(tempHolder).load(thisUrl + " #results", function(){
+		$(thisArea).append($(".summaryDataRow li"));
+		if ($(tempHolder).text().indexOf("NOMORERECORDS") > -1){
+			console.log("NUMBER OF RECORDS is > -1");
+			$(obj).nextAll(".summaryDataRow:first").children(".loadMoreLink").hide();
+		}else{
+			console.log("NUMBER OF RECORDS is -1");
+			$(obj).nextAll(".summaryDataRow:first").children(".loadMoreLink").show();	
+		}
+		$(tempHolder).empty();
+	});
+	
+	
+	$(obj).addClass("accordianExpanded");
+	$(obj).nextAll(".summaryDataRow:first").children(".loadMoreLink").show();
+
+}
+
+function fetchDetails(obj, viewName, catName, xpage)
 {	
-	//console.log('View: ' + viewName);
-	//console.log('Category: ' + catName);
+	console.log('View: ' + viewName);
+	//console.log("Object = " + obj);
+	//Reset category
+	$('.accordionRowSet').empty();
+	$('.loadMoreLink').hide();
+	
+	console.log('Category: ' + catName);
 	if($(obj).hasClass("accordianExpanded")){
-		//$(obj).nextAll('.summaryDataRow:first').children('#results').remove();
-		$(obj).nextAll('.summaryDataRow:first').children('#results').slideUp('slow', function(){ $(this).remove()});
+		$(obj).nextAll('.summaryDataRow:first').children('.accordionRowSet').slideUp('fast', function(){ $(this).children().remove()});
 		$(obj).removeClass("accordianExpanded");
+		$(obj).nextAll('.summaryDataRow:first').children('.loadMoreLink').hide();
 	}
 	else{
-		var thisArea = $(obj).next(".summaryDataRow");
-		encodedCat = encodeURIComponent(catName);
-		thisArea.load("UnpAccordionViewList.xsp?chosenView=" + viewName + "&catFilter=" + encodedCat + " #results");
-		$(obj).addClass("accordianExpanded");
+		accordionLoadMore(obj, viewName, catName, xpage);
 	}
 }
-function fetchDoc(thisUnid){
-	window.location("/SafetySnapshotChecklist.xsp?action=openDocument&documentId=" + thisUnid);	
+
+function fetchMoreDetails(obj, viewName, catName, xpage){
+	
+	console.log("Load more button tapped...");
+	var objRow = $(obj).parent().parent().prev();
+	accordionLoadMore(objRow, viewName, catName, xpage);	
 }
