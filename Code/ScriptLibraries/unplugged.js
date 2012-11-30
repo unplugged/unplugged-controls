c4a4cfb8c04b18834e865893a49657f65a5209de
Matt White <matthew.white@fclonline.com>
2012-11-30 10:54:37 +0000

    Fixed #16 and Fixed #21

--- Code/ScriptLibraries/unplugged.js
+++ Code/ScriptLibraries/unplugged.js
@@ -1,7 +1,6 @@
 $(window).load( function() {
 
 	$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
-	$(document).ajaxStop(initiscroll);
 	allowFormsInIscroll();
 	$('.viewsButton').unbind('click');
 	$('.viewsButton').click( function(event) {
@@ -317,4 +316,63 @@ function closeDialog(id){
 	$(id).css('display', 'none');
 	$("#adminCover").css('display', 'none');
 	initiscroll();
+}
+
+
+
+function accordionLoadMore(obj, viewName, catName, xpage){
+	
+	var thisArea = $(obj).nextAll(".summaryDataRow:first").children(".accordionRowSet");		
+	console.log("thisArea = " + $(thisArea).attr('class'));
+	var pos = $(thisArea).find('li').length;
+	console.log("pos length= " + $(thisArea).find('li').length);
+	thisArea.css('display','block');
+	var thisUrl = "UnpAccordionViewList.xsp?chosenView=" + encodeURIComponent(viewName) + "&catFilter=" + encodeURIComponent(catName) + "&xpageDoc=" + xpage + "&start=" + pos; 
+	
+	var tempHolder = $(obj).nextAll(".summaryDataRow:first").children(".summaryDataRowHolder");
+	$(tempHolder).load(thisUrl + " #results", function(){
+		$(thisArea).append($(".summaryDataRow li"));
+		if ($(tempHolder).text().indexOf("NOMORERECORDS") > -1){
+			console.log("NUMBER OF RECORDS is > -1");
+			$(obj).nextAll(".summaryDataRow:first").children(".loadMoreLink").hide();
+		}else{
+			console.log("NUMBER OF RECORDS is -1");
+			$(obj).nextAll(".summaryDataRow:first").children(".loadMoreLink").show();	
+		}
+		$(tempHolder).empty();
+		try{
+			initiscroll();
+		}catch(e){}
+	});
+	
+	
+	$(obj).addClass("accordianExpanded");
+	$(obj).nextAll(".summaryDataRow:first").children(".loadMoreLink").show();
+
+}
+
+function fetchDetails(obj, viewName, catName, xpage)
+{	
+	console.log('View: ' + viewName);
+	//console.log("Object = " + obj);
+	//Reset category
+	$('.accordionRowSet').empty();
+	$('.loadMoreLink').hide();
+	
+	console.log('Category: ' + catName);
+	if($(obj).hasClass("accordianExpanded")){
+		$(obj).nextAll('.summaryDataRow:first').children('.accordionRowSet').slideUp('fast', function(){ $(this).children().remove()});
+		$(obj).removeClass("accordianExpanded");
+		$(obj).nextAll('.summaryDataRow:first').children('.loadMoreLink').hide();
+	}
+	else{
+		accordionLoadMore(obj, viewName, catName, xpage);
+	}
+}
+
+function fetchMoreDetails(obj, viewName, catName, xpage){
+	
+	console.log("Load more button tapped...");
+	var objRow = $(obj).parent().parent().prev();
+	accordionLoadMore(objRow, viewName, catName, xpage);	
 }
