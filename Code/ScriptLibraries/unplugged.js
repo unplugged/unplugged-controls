f0f3e38bc7f464d08746d4cda32249d667bd7922
Matt White <matthew.white@fclonline.com>
2013-03-22 14:04:47 +0000

    Fixed bug with unplugged.js

--- Code/ScriptLibraries/unplugged.js
+++ Code/ScriptLibraries/unplugged.js
@@ -10,11 +10,13 @@
 
 $(window).load( function() {
 	if (!unpluggedserver){
-		for ( i=0; i<document.styleSheets.length; i++) {
-			if (document.styleSheets.item(i).href.indexOf("defaultTheme.css") > -1 || document.styleSheets.item(i).href.indexOf("core.css") > -1){
-				void(document.styleSheets.item(i).disabled=true);
+		try {
+			for ( i=0; i<document.styleSheets.length; i++) {
+				if (document.styleSheets.item(i).href.indexOf("defaultTheme.css") > -1 || document.styleSheets.item(i).href.indexOf("core.css") > -1){
+					void(document.styleSheets.item(i).disabled=true);
+				}
 			}
-		}
+		}catch(e){}
 	}
 	
 	$(document).ajaxStart($.blockUI).ajaxStop($.unblockUI);
