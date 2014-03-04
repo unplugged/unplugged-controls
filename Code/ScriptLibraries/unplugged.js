39b82c03cfeddd06dd77bd7f797083a8c6209b3b
Matt White <matthew.white@fclonline.com>
2014-03-04 11:51:25 +0000

    Fix #371
    Signed-off-by: Matt White <matthew.white@fclonline.com>

--- Code/ScriptLibraries/unplugged.js
+++ Code/ScriptLibraries/unplugged.js
@@ -859,13 +859,25 @@ unp.increaseFontSize = function(button) {
 unp.decreaseFontSize = function(button) {
 	$(".typographyreadcontent").find("*").each(
 		function() {
-			$(this).css("font-size",
-					(parseInt($(this).css("font-size"), 10) - 2) + "px");
-			if (parseInt($(this).css("line-height"), 10) > 24) {
-				$(this).css(
-						"line-height",
-						(parseInt($(this).css("line-height"), 10) - 2)
-								+ "px");
+			var tagName = $(this).prop("tagName");
+			var fontSize = parseInt($(this).css("font-size"), 10);
+			var minFontSize = 4;
+			if (tagName == "H1"){
+				minFontSize = 28;
+			}else if(tagName == "H2"){
+				minFontSize = 24;
+			}else if(tagName == "H3"){
+				minFontSize = 18;
+			}else if(tagName == "H4"){
+				minFontSize = 12;
+			}else if(tagName == "H5"){
+				minFontSize = 8;
+			}
+			if (fontSize - 2 >= minFontSize){
+				$(this).css("font-size", (fontSize - 2) + "px");
+				if (parseInt($(this).css("line-height"), 10) > 24) {
+					$(this).css("line-height", (parseInt($(this).css("line-height"), 10) - 2) + "px");
+				}
 			}
 		}
 	);
